54bfa498aaa51abe402e911a4ec603491ba589ed
stefhan235 <stefhansalim12@gmail.com>
2024-04-19 20:38:41 +0700

    update config database

--- config/database.js
+++ config/database.js
@@ -13,13 +13,13 @@ module.exports = {
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DATABASE,
         host: process.env.POSTGRES_HOST,
-        dialect: "mysql",
+        dialect: "postgres",
     },
     production: {
         username: process.env.POSTGRES_USERNAME,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DATABASE,
         host: process.env.POSTGRES_HOST,
-        dialect: "mysql",
+        dialect: "postgres",
     },
 };
